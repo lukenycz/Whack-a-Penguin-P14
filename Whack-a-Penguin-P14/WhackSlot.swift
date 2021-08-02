87489a4beccd46549c73b02bdc3ee54f46afd0c9
Łukasz Nycz <lnycz96@gmail.com>
2021-08-02 12:54:16 +0200

    Penguin, show thyself: SKAction moveBy(x:y:duration:)

--- Whack-a-Penguin-P14/WhackSlot.swift
+++ Whack-a-Penguin-P14/WhackSlot.swift
@@ -11,6 +11,9 @@ import SpriteKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -24,8 +27,35 @@ class WhackSlot: SKNode {
         
         charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
         cropNode.addChild(charNode)
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() - (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible {return}
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
     
 
